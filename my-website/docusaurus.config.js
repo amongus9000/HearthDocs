cdbe46290028ce096d44e0b12ecf4385bd3d4a83
Simonee <67618616+simoneedotexe@users.noreply.github.com>
2021-06-10 03:40:25 +0800

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://simoneedotexe.github.io/',
+  baseUrl: '/HearthDocs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'simoneedotexe', // Usually your GitHub org/user name.
+  projectName: 'HearthDocs', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
@@ -25,7 +25,7 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/simoneedotexe/HearthDocs',
           label: 'GitHub',
           position: 'right',
         },
@@ -85,13 +85,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/simoneedotexe/HearthDocs/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/simoneedotexe/HearthDocs/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
