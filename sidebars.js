64d37a16ba2f5f7b2230a7961829ad6ff285967b
Simonee <67618616+simoneedotexe@users.noreply.github.com>
2021-07-06 17:56:34 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -20,6 +20,54 @@ module.exports = {
             label: 'Discord',
             href: 'https://discord.hearthcraft.net/',
         },
+        
+        {
+            type: 'category',
+            label: 'Other Links',
+            collapsed: false,
+            items: [
+                  {
+                     type: 'link',
+                     label: 'Webstore',
+                     href: 'https://store.hearthcraft.net/',
+                  },
+                  {
+                     type: 'link',
+                     label: 'Dynamic Map',
+                     href: 'https://map.hearthcraft.net/',
+                  },
+                  {
+                     type: 'link',
+                     label: 'Subreddit',
+                     href: 'https://reddit.com/r/HearthCraft/',
+                  },
+                  {
+                     type: 'link',
+                     label: 'Instagram',
+                     href: 'https://instagram.com/HearthCraft/',
+                  },
+                  {
+                     type: 'link',
+                     label: 'Statistics',
+                     href: 'https://calypso.hearthcraft.net/',
+                  },
+                  {
+                     type: 'link',
+                     label: 'Icon Pack',
+                     href: 'http://cdn.moep.tv/files/Custom_Icons_Resource_Pack_small.zip',
+                  },
+                  {
+                     type: 'link',
+                     label: 'Trailer',
+                     href: 'https://www.youtube.com/watch?v=tKJ4dBRGOsc&t=27s',
+                  },
+                  {
+                     type: 'link',
+                     label: 'Voting',
+                     href: 'https://hearthcraft.net/vote',
+                  },
+            ],
+        },
 
         {
             type: 'category',
